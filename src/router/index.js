3a9be8a89cecb9a50e5d65178cfe569341499d2c
oviliansyah11 <oviliansyah11@gmail.com>
2021-12-13 18:16:29 +0700

    convert html to vuejs

---
+++ src/router/index.js
@@ -0,0 +1,36 @@
+import Vue from 'vue'
+import VueRouter from 'vue-router'
+import Home from '../views/Home.vue'
+import Product from '../views/Product.vue'
+import Cart from '../views/Cart.vue'
+import Success from '../views/Success.vue'
+
+Vue.use(VueRouter)
+
+const routes = [{
+    path: '/',
+    name: 'Home',
+    component: Home
+  },
+  {
+    path: '/product',
+    name: 'Product',
+    component: Product
+  },
+  {
+    path: '/cart',
+    name: 'Cart',
+    component: Cart
+  },
+  {
+    path: '/success',
+    name: 'Success',
+    component: Success
+  }
+]
+
+const router = new VueRouter({
+  routes
+})
+
+export default router
---
+++ src/views/Home.vue
@@ -0,0 +1,32 @@
+<template>
+  <div class="home">
+    <Header />
+    <Hero />
+    <Banner />
+    <SectionIG />
+    <Logo />
+    <Footer />
+  </div>
+</template>
+
+<script>
+// @ is an alias to /src
+import Header from "@/components/Header.vue";
+import Hero from "@/components/Hero.vue";
+import Banner from "@/components/Banner.vue";
+import SectionIG from "@/components/SectionIG.vue";
+import Logo from "@/components/Logo.vue";
+import Footer from "@/components/Footer.vue";
+
+export default {
+  name: "Home",
+  components: {
+    Header,
+    Hero,
+    Banner,
+    SectionIG,
+    Logo,
+    Footer,
+  },
+};
+</script>
