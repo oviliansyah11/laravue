a1ac495147efdf5650ab5b047302d8a29b0ecf54
oviliansyah11 <oviliansyah11@gmail.com>
2021-12-14 12:01:05 +0700

    Slicing dashboard admin

--- backend-laravue/routes/web.php
+++ backend-laravue/routes/web.php
@@ -1,5 +1,7 @@
 <?php
 
+use App\Http\Controllers\DashboardController;
+use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -13,9 +15,7 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
-});
+Route::get('/', [DashboardController::class, 'index']);
 
 Auth::routes();
 
